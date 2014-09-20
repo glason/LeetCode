380ec66f62dce6e537638b547b0948e1716ef3c3
jiasheng <jiasheng55@gmail.com>
2014-09-20 16:40:32 +0800

    set vim shiftwidth=4

--- Valid Parentheses.java
+++ Valid Parentheses.java
@@ -4,26 +4,26 @@
  * The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
  */
 public class Solution {
-	public boolean isValid(String s) {
-		Stack<Character> stack = new Stack<Character>();
-		for(int i=0;i<s.length();i++){
-			char c = s.charAt(i);
-			if(c=='('||c=='{'||c=='['){
-				stack.push(c);
-			}else{
-				if(stack.isEmpty()){
-					return false;
-				}
-				char c2=stack.pop();
-				if((c==')'&&c2!='(')||(c=='}'&&c2!='{')||(c==']'&&c2!='[')){
-					return false;  
-				}
-			}
-		}
+    public boolean isValid(String s) {
+	Stack<Character> stack = new Stack<Character>();
+	for(int i=0;i<s.length();i++){
+	    char c = s.charAt(i);
+	    if(c=='('||c=='{'||c=='['){
+		stack.push(c);
+	    }else{
 		if(stack.isEmpty()){
-			return true;
-		}else{
-			return false;
+		    return false;
 		}
+		char c2=stack.pop();
+		if((c==')'&&c2!='(')||(c=='}'&&c2!='{')||(c==']'&&c2!='[')){
+		    return false;  
+		}
+	    }
 	}
+	if(stack.isEmpty()){
+	    return true;
+	}else{
+	    return false;
+	}
+    }
 }
